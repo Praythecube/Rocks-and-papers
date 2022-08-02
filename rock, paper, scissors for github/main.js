e3b0347d8872c3e44265688f7a9112e688be7832
Charlie Appleton <xjarlie@gmail.com>
2022-08-02 14:02:05 +0100

    logic rewrite

--- rock, paper, scissors for github/main.js
+++ rock, paper, scissors for github/main.js
@@ -12,38 +12,70 @@ myButton.onclick = function() {
     random = getRandomInt(3);
     userInput();
 
-    if (input === items[random]) {
-        alert("Nobody wins!");
-        userImage.setAttribute("src", "images/" + input + ".jpeg");
-        randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
-    } else if (input === "rock" && items[random] === "paper") {
-        alert("You lose.");
-        userImage.setAttribute("src", "images/" + input + ".jpeg");
-        randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
-    } else if (input === "rock" && items[random] === "scissors") {
-        alert("You win!");
-        userImage.setAttribute("src", "images/" + input + ".jpeg");
-        randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
-    } else if (input === "paper" && items[random] === "scissors") {
-        alert("You lose.");
-        userImage.setAttribute("src", "images/" + input + ".jpeg");
-        randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
-    } else if (input === "paper" && items[random] === "rock") {
-        alert("You win!");
-        userImage.setAttribute("src", "images/" + input + ".jpeg");
-        randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
-    } else if (input === "scissors" && items[random] === "rock") {
-        alert("You lose.");
-        userImage.setAttribute("src", "images/" + input + ".jpeg");
-        randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
-    } else if (input === "scissors" && items[random] === "paper") {
-        alert("You win!");
-        userImage.setAttribute("src", "images/" + input + ".jpeg");
-        randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
-    } else {
-        alert("No imput.")
-    }
+    // if (input === items[random]) {
+    //     alert("Nobody wins!");
+    //     userImage.setAttribute("src", "images/" + input + ".jpeg");
+    //     randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
+    // } else if (input === "rock" && items[random] === "paper") {
+    //     alert("You lose.");
+    //     userImage.setAttribute("src", "images/" + input + ".jpeg");
+    //     randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
+    // } else if (input === "rock" && items[random] === "scissors") {
+    //     alert("You win!");
+    //     userImage.setAttribute("src", "images/" + input + ".jpeg");
+    //     randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
+    // } else if (input === "paper" && items[random] === "scissors") {
+    //     alert("You lose.");
+    //     userImage.setAttribute("src", "images/" + input + ".jpeg");
+    //     randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
+    // } else if (input === "paper" && items[random] === "rock") {
+    //     alert("You win!");
+    //     userImage.setAttribute("src", "images/" + input + ".jpeg");
+    //     randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
+    // } else if (input === "scissors" && items[random] === "rock") {
+    //     alert("You lose.");
+    //     userImage.setAttribute("src", "images/" + input + ".jpeg");
+    //     randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
+    // } else if (input === "scissors" && items[random] === "paper") {
+    //     alert("You win!");
+    //     userImage.setAttribute("src", "images/" + input + ".jpeg");
+    //     randomImage.setAttribute("src", "images/" + items[random] +  ".jpeg");
+    // } else {
+    //     alert("No imput.")
+    // }
 
+    let alertText;
+    switch (input) {
+        case items[random]:
+            alertText = 'Nobody wins!';
+            break;
+        case 'rock':
+            if (items[random] === 'paper') {
+                alertText = 'You lose.';
+            } else {
+                alertText = 'You win!';
+            }
+            break;
+        case 'paper':
+            if (items[random] === 'scissors') {
+                alertText = 'You lose.';
+            } else {
+                alertText = 'You win';
+            }
+            break;
+        case 'scissors':
+            if (items[random] === 'rock') {
+                alertText = 'You lose.';
+            } else {
+                alertText = 'You win!';
+            }
+            break;
+        default:
+            alertText = 'No input.';
+    }
+    userImage.setAttribute('src', 'images/' + input + '.jpeg');
+    randomImage.setAttribute('src', 'images/' + items[random] + '.jpeg')
+    alert(alertText);
 
 }
 
